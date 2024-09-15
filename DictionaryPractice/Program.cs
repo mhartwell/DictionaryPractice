ab9d69169ca3cffbc987715a8df343b53aa6120c
Mike Hartwell <mhartwell1988@gmail.com>
2024-09-15 08:08:17 -0500

    first commit

---
+++ DictionaryPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections;
+
+int[] input = new int[]{2,4,6,1,4,7};
+int target = 9;
+
+var pairs = FindPairs(input, target);
+foreach(var pair in pairs){
+    Console.WriteLine($"Pair: {pair[0]}, {pair[1]}");
+}
+IList<IList<int>> FindPairs(int[] nums, int target){
+    IList<IList<int>> result = new List<IList<int>>();
+    HashSet<int> seenNumbers = new HashSet<int>();
+
+    foreach (var num in nums){
+        int complement = target - num;
+        if(seenNumbers.Contains(complement)){
+            result.Add(new List<int> {complement, num});
+        }
+        seenNumbers.Add(num);
+    }
+    return result;
+}
